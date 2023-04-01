69f136f5abd8b45aaaf2e9975ad8241f907eff63
Metodi Simeonov <47445653+Met0o@users.noreply.github.com>
2023-04-01 16:23:14 +0300

    Add files via upload

---
+++ Video-Stream.py
@@ -0,0 +1,40 @@
+import cv2
+from flask import Flask, Response, render_template
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def generate_frames():
+    camera = cv2.VideoCapture(0)
+
+    # Set the resolution
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    # Set the refresh rate (fps)
+    camera.set(cv2.CAP_PROP_FPS, 30)
+
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            # Set the video stream quality (JPEG compression)
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 100]  # 80 is the quality value (0-100)
+            ret, buffer = cv2.imencode('.jpg', frame, encode_param)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        time.sleep(1 / 30)  # Sleep for the inverse of the fps to achieve the desired refresh rate
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
