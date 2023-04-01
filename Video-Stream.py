67e90b181032b30d0e69c9f7b6d07babbdcc8316
Metodi Simeonov <metodi.simeonov@gmail.com>
2023-04-01 16:29:29 +0300

    code updates

--- Video-Stream.py
+++ Video-Stream.py
@@ -12,8 +12,8 @@ def generate_frames():
     camera = cv2.VideoCapture(0)
 
     # Set the resolution
-    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) #1280
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720) #720
 
     # Set the refresh rate (fps)
     camera.set(cv2.CAP_PROP_FPS, 30)
@@ -23,13 +23,12 @@ def generate_frames():
         if not success:
             break
         else:
-            # Set the video stream quality (JPEG compression)
-            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 100]  # 80 is the quality value (0-100)
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
             ret, buffer = cv2.imencode('.jpg', frame, encode_param)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        time.sleep(1 / 30)  # Sleep for the inverse of the fps to achieve the desired refresh rate
+        #time.sleep(1 / 30)  # Sleep for the inverse of the fps to achieve the desired refresh rate
 
 @app.route('/video_feed')
 def video_feed():
